9ebe99a72f8ccbf97918827a45bf26899e370a5c
kgrammont <kgrammont@ippon.fr>
2021-06-20 13:19:29 +0200

    Initial version of QCMManager generated by JHipster-7.0.1

---
+++ src/test/java/com/qcmmanager/ArchTest.java
@@ -0,0 +1,29 @@
+package com.qcmmanager;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.qcmmanager");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.qcmmanager.service..")
+            .or()
+            .resideInAnyPackage("com.qcmmanager.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.qcmmanager.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
