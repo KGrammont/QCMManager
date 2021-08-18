542c1d2302085c7f47aa7528dd4aa5b70ec08fcc
kgrammont <kgrammont@ippon.fr>
2021-08-18 19:29:23 +0200

    Try email

--- src/main/java/com/qcmmanager/service/MailService.java
+++ src/main/java/com/qcmmanager/service/MailService.java
@@ -53,7 +53,7 @@ public class MailService {
 
     @Async
     public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-        log.debug(
+        log.info(
             "Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
             isMultipart,
             isHtml,
